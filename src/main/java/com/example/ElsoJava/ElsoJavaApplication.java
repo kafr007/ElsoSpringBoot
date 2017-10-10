6b690e109f2b584b9811aa655f35e84d7c126355
Franciska Kando <kafr007@gmail.com>
2017-10-10 14:16:32 +0200

    Elso commit

---
+++ src/main/java/com/example/ElsoJava/ElsoJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.ElsoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsoJavaApplication.class, args);
+	}
+}
